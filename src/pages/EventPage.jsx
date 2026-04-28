c69ef8b1e762b97957d490a6aaec92ed74c6164d
angelinagut12 <angelinagut12@gmail.com>
2026-04-28 01:00:19 -0500

    force update to trigger deploy

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+// FORCE UPDATE TEST
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { supabase } from "../components/supabaseClient";
