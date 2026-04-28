c6031285914c9ef78d3fc9b4f5297b73682a21a3
angelinagut12 <angelinagut12@gmail.com>
2026-04-28 02:03:00 -0500

    Fix merge conflicts and build errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-function Footer() {
-  return (
-    <div
-      style={{
-        marginTop: "2rem",
-        textAlign: "center",
-        fontSize: "0.85rem",
-        color: "#777",
-      }}
-    >
-      © {new Date().getFullYear()} InvitePool by Angelina Gutierrez
-    </div>
-  );
-}
-
-=======
 function Footer() {
   return (
     <div
@@ -30,5 +13,4 @@ function Footer() {
   );
 }
 
->>>>>>> 89393c1 (Initial commit with email system)
 export default Footer;
